28d876b4723744a3a52ddce34f281b80a61cc9f9
Sheshank2805 <126772292+Sheshank2805@users.noreply.github.com>
2023-12-08 13:01:00 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const TodoList = ({ todo, todos, setTodos }) => {
+const TodoItem = ({ todo, todos, setTodos }) => {
   const [editing, setEditing] = useState(false);
   const [editedTitle, setEditedTitle] = useState(todo.title);
 
@@ -34,52 +34,58 @@ const TodoList = ({ todo, todos, setTodos }) => {
   };
 
   return (
-    <div className="flex justify-center mt-2 px-2">
-      {editing ? (
-        <form onSubmit={handleEditSubmit}>
-          <input
-            type="text"
-            value={editedTitle}
-            onChange={handleEditChange}
-            className="mr-2 rounded-lg"
-          />
-          <button
-            type="submit"
-            className="text-gray-300 bg-red-500 rounded-2xl p-3 hover:bg-orange-400"
-          >
-            Save
-          </button>
-        </form>
-      ) : (
-        <div>
+    <div className="flex justify-between items-center mt-2 px-2">
+      <div className="flex items-center">
+        {editing ? (
+          <form onSubmit={handleEditSubmit} className="flex items-center">
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={handleEditChange}
+              className="mr-2 rounded-lg"
+            />
+            <button
+              type="submit"
+              className="text-gray-300 bg-red-500 rounded-2xl p-3 hover:bg-orange-400"
+            >
+              Save
+            </button>
+          </form>
+        ) : (
           <span
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
             className="px-2 text-gray-100 md:w-full sm:w-full"
           >
             {todo.title}
           </span>
-          <input
-            className="mr-2"
-            type="checkbox"
-            checked={todo.completed}
-            onChange={handleCompletedToggle}
-          />
-          <button
-            onClick={handleEdit}
-            className="text-gray-300 px-2 bg-slate-950 rounded-3xl hover:bg-blue-800 p-2 mr-2"
-          >
-            Edit
-          </button>
-          <button
-            onClick={handleDelete}
-            className="text-gray-300 bg-cyan-950 rounded-2xl p-2 hover:bg-red-600"
-          >
-            Delete
-          </button>
-        </div>
-      )}
+        )}
+      </div>
+      <div className="flex">
+        {!editing && (
+          <>
+            <input
+              className="mr-4"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={handleCompletedToggle}
+            />
+            <button
+              onClick={handleEdit}
+              className="text-gray-300 px-2 bg-slate-950 rounded-3xl hover:bg-blue-800 p-2 mr-4"
+            >
+              Edit
+            </button>
+            <button
+              onClick={handleDelete}
+              className="text-gray-300 bg-cyan-950 rounded-2xl p-2 hover:bg-red-600"
+            >
+              Delete
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
-export default TodoList;
+export default TodoItem;
